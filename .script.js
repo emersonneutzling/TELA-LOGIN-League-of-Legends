3858e3cb2bd2fafc57d014956d5e1590420fb6cd
Emerson Moreira Neutzling <emerson.neutzling@gmail.com>
2023-02-28 15:42:24 -0300

    atualizei javascript

---
+++ .script.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('.input');
+
+const handleFocus = ({ target }) => {
+    const span = target.previousElementSibling;
+    span.classList.add('span-active');
+   
+
+}
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus))
