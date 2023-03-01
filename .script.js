5c96b4058508e9ef4de479454ef1e459dc47f2a3
Emerson Moreira Neutzling <emerson.neutzling@gmail.com>
2023-03-01 10:55:56 -0300

    atualizei projeto

--- .script.js
+++ .script.js
@@ -4,7 +4,17 @@ const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
     span.classList.add('span-active');
    
+}
+
+const handleFocusOut = ({ target }) => {
+    if (target.value == '') {
+        const span = target.previousElementSibling;
+    span.classList.remove('span-active');
+
+    }
+    
 
 }
 
-inputs.forEach((input) => input.addEventListener('focus', handleFocus))
+inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
